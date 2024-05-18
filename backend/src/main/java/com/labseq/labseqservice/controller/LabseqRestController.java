00850cdbe966078dd7f17ca8fd26e1ddc0a35d09
Iago América <90301260+Iago-America@users.noreply.github.com>
2024-05-18 03:08:05 +0100

    Fix getLabseqResult with ResponseEntity.

--- backend/src/main/java/com/labseq/labseqservice/controller/LabseqRestController.java
+++ backend/src/main/java/com/labseq/labseqservice/controller/LabseqRestController.java
@@ -2,12 +2,15 @@ package com.labseq.labseqservice.controller;
 
 import com.labseq.labseqservice.service.LabseqService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.math.BigInteger;
+import java.util.Map;
 
 @RestController
 @RequestMapping("labseq")
@@ -17,9 +20,9 @@ public class LabseqRestController {
     private LabseqService labseqService;
 
     @GetMapping("/{number}")
-    public BigInteger getLabseqResult(@PathVariable("number") int number) {
+    public ResponseEntity<BigInteger> getLabseqResult(@PathVariable("number") int number) {
         BigInteger result = labseqService.getLabseqValue(number);
-        return labseqService.getLabseqValue(number);
+        return new ResponseEntity<>(result, HttpStatus.OK);
     }
 
 }
