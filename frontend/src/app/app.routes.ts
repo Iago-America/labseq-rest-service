3c654c3d6588391fd92fd9946deae9525072356a
Iago América <90301260+Iago-America@users.noreply.github.com>
2024-05-18 09:47:02 +0100

    Configure HttpClient and route

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { LabseqResultComponent } from './components/LabseqResult/labseq-result/labseq-result.component';
+import { HomeComponent } from './components/Home/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'search', component: LabseqResultComponent }
+];
