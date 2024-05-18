c6eb8edce34844d5fdf8e673e173dc273dfec867
Iago América <90301260+Iago-America@users.noreply.github.com>
2024-05-18 04:21:03 +0100

    Add @CrossOrigin annotation enable CORS.

--- backend/src/main/java/com/labseq/labseqservice/controller/LabseqRestController.java
+++ backend/src/main/java/com/labseq/labseqservice/controller/LabseqRestController.java
@@ -4,13 +4,10 @@ import com.labseq.labseqservice.service.LabseqService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.math.BigInteger;
-
+@CrossOrigin(origins = {"*"})
 @RestController
 @RequestMapping("labseq")
 public class LabseqRestController {
