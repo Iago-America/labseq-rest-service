6b1e3168ea68e4b4cb989e1767e5b9d2c1bf78cd
Iago América <90301260+Iago-America@users.noreply.github.com>
2024-05-18 08:51:52 +0100

    Add @ApiResponses annotations in endpoint for Swagger.

--- backend/src/main/java/com/labseq/labseqservice/controller/LabseqRestController.java
+++ backend/src/main/java/com/labseq/labseqservice/controller/LabseqRestController.java
@@ -1,13 +1,18 @@
 package com.labseq.labseqservice.controller;
 
 import com.labseq.labseqservice.service.LabseqService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.math.BigInteger;
-@CrossOrigin(origins = {"*"})
+//@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("labseq")
 public class LabseqRestController {
@@ -15,6 +20,16 @@ public class LabseqRestController {
     @Autowired
     private LabseqService labseqService;
 
+
+    @Operation(
+            summary = "Retrieve a Number of Labseq.",
+            description =
+                    "Get a Number of Labseq and published status.")
+    @ApiResponses({
+            @ApiResponse( responseCode = "200", content = { @Content(schema = @Schema(implementation = String.class)) }),
+            @ApiResponse( responseCode = "404", content = { @Content(schema = @Schema()) }),
+            @ApiResponse( responseCode = "500",content = { @Content(schema = @Schema()) })
+    })
     @GetMapping("/{number}")
     public ResponseEntity<String> getLabseqResult(@PathVariable("number") int number) {
         BigInteger result = labseqService.getLabseqValue(number);
